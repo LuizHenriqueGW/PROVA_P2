c9be60f2b91df979411defcfd0b5f57c49cdf9a2
Kaiser <luiz_cs421@hotmail.com>
2020-11-23 19:57:21 -0400

    Segundo Commit

--- app/src/main/java/com/example/prova_p2/MainActivity.java
+++ app/src/main/java/com/example/prova_p2/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.prova_p2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText n;
+    private EditText i;
+    private Dao Pessoa;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        n=findViewById(R.id.cp_Nome);
+        i=findViewById(R.id.cp_Idade);
+    }
+    public void salvar(View view){
+        Pessoa.setNome(n.getText().toString());
+        Pessoa.setIdade(Integer.parseInt(i.getText().toString()));
     }
 }
